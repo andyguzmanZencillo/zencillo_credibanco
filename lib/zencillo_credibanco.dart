898fe6a2af2e3be316bbf332b90a74a603a70202
andyguzmanZencillo <166416095+andyguzmanZencillo@users.noreply.github.com>
2026-06-22 11:40:54 -0400

    add response model credibanco

--- lib/zencillo_credibanco.dart
+++ lib/zencillo_credibanco.dart
@@ -1,4 +1,6 @@
 import 'package:oxidized/oxidized.dart';
+import 'package:zencillo_credibanco/map/map.dart';
+import 'package:zencillo_helpers/zencillo_helpers.dart';
 
 import 'models/credibanco_response.dart';
 import 'zencillo_credibanco_platform_interface.dart';
@@ -16,6 +18,34 @@ class ZencilloCredibanco {
       amount: amount,
       tax: tax,
     );
+  }
+
+  Future<Result<FormaPagoDetalleModel, String>> credibancoPayFull({
+    required int idTurno,
+    required int numeroTurno,
+    required int idDocument,
+    required double total,
+    required double taxTotal,
+    required double subTotal,
+    required int idFormaPago,
+  }) async {
+    final result = await ZencilloCredibancoPlatform.instance.credibanco(
+      amount: total.toString(),
+      tax: taxTotal.toString(),
+    );
+    if (result.isErr()) {
+      return Err(result.unwrapErr());
+    }
+    final data = result.unwrap();
+    return Ok(data.toFormaPagoDetalle(
+      idTurno: idTurno,
+      numeroTurno: numeroTurno,
+      idDocument: idDocument,
+      total: total,
+      taxTotal: taxTotal,
+      subTotal: subTotal,
+      idFormaPago: idFormaPago,
+    ));
   }
 
   Future<Result<Unit, String>> credibancoPrint(String text) {
